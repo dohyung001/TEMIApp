1d03f12a022c629000beb2483d12e62c8b9a0341
dohyung <154952683+dohyung001@users.noreply.github.com>
2025-10-27 21:03:33 +0900

    [Feat]: 잘 돌아가도록

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,9 +1,17 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        format: "iife", // 즉시 실행 형식 (웹뷰 호환)
+        entryFileNames: "index.js",
+      },
+    },
+  },
 });
